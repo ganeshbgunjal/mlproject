6093fb25d6781b1ac81398344ad25f68ee2ee469
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2025-11-08 10:18:28 +0530

    data ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
 import sys  # allows you to interact with standard input, output and error streams.
 from types import TracebackType
-from logger import logging
+from src.logger import logging
 
 
 def error_message_detail(error,error_details:sys):
