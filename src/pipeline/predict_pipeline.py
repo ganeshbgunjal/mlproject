8e9d7c8cf2195aa1572aaf32bcdbb265ac80afe7
ganeshgunjal118@gmail.com <ganeshgunjal118@gmail.com>
2025-11-15 22:50:17 +0530

    prediction pipeline added

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,66 @@
+import sys
+import pandas as pd
+import numpy as np
+from src.exception import CustomException
+from src.utils import load_object 
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):  #method to make predictions.
+        try:
+            model_path = 'artifacts/model.pkl'  #path to the saved model.
+            preprocessor_path = 'artifacts/preprocessor.pkl'  #path to the saved preprocessor.
+
+            model = load_object(file_path=model_path)  #load the model.
+            preprocessor = load_object(file_path=preprocessor_path)  #load the preprocessor.
+
+            data_scaled = preprocessor.transform(features)  #preprocess the input features.
+
+            preds = model.predict(data_scaled)  #make predictions.
+
+            return preds
+        except Exception as e:
+            raise CustomException(e, sys)
+
+class CustomData:    #class to handle user input data.
+    def __init__( self,
+        gender: str,
+        race_ethnicity: str,
+        parental_level_of_education,
+        lunch: str,
+        test_preparation_course: str,
+        reading_score: int,
+        writing_score: int
+        ):
+        self.gender = gender
+        self.race_ethnicity = race_ethnicity
+        self.parental_level_of_education = parental_level_of_education
+        self.lunch = lunch
+        self.test_preparation_course = test_preparation_course
+        self.reading_score = reading_score
+        self.writing_score = writing_score
+
+    def get_data_as_data_frame(self):  #method to convert user input data to pandas dataframe.
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.race_ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "reading_score": [self.reading_score],
+                "writing_score": [self.writing_score]
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+            raise CustomException(e, sys)
+        
+
+
+
+
+
--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import os
 import sys
 import dill
 from sklearn.metrics import r2_score
+import pickle
 
 from src.exception import CustomException
 from src.logger import logging
@@ -54,3 +55,11 @@ def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     
     except Exception as e:
         raise CustomException(e,sys)  #raise custom exception.  
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
